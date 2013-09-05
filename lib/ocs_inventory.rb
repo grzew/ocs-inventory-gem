ecd0f07b2439fe9eacbc1e4e1845a27c6daaf136
Grzegorz Wieczorek <grzewster@gmail.com>
2013-09-05 11:30:36 +0200

    Added office KEYS

--- lib/ocs_inventory.rb
+++ lib/ocs_inventory.rb
@@ -27,9 +27,20 @@ class OcsInventory
 #  1 - Windows KEY
 #  2 - Microfoft Office
 
-  def self.get_license(type,interval)
+  def self.get_WINlicense(interval)
+    self.connect
     begin
       results = @db.query "select NAME,LASTCOME,WINOWNER,OSNAME,WINPRODKEY from hardware where LASTCOME > TIMESTAMP(DATE_SUB(NOW(), INTERVAL #{interval} day))"
+      return results
+    ensure
+      @db.close
+    end
+  end
+
+  def self.get_OFFICElicense(version)
+    self.connect
+    begin
+      results = @db.query "select HARDWARE_ID,PRODUCT,OFFICEKEY from officepack where OFFICEVERSION = #{version}"
       return results
     ensure
       @db.close
